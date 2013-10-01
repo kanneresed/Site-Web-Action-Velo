cd7bc5ab47ccbfd824101b4b7f31f93199eb1809
luois <lpink_11@hotmail.com>
2013-09-30 17:21:43 -0700

    first commit branch louis

--- Index.php
+++ Index.php
@@ -1,8 +1,196 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: administrateur
- * Date: 13-09-26
- * Time: 21:45
- * To change this template use File | Settings | File Templates.
- */
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=9,chrome=1"/>
+    <meta charset="utf-8"/>
+    <title>site-action-citoyen</title>
+    <meta name="fb_admins_meta_tag" content=""/>
+    <link rel="shortcut icon" href="http://www.wix.com/favicon.ico" type="image/png"/>
+    <link rel="canonical" href="http://louissimard11.wix.com/site-action-citoyen"/>
+    <meta http-equiv="X-Wix-Renderer-Server" content="app32.aus"/>
+    <meta http-equiv="X-Wix-Meta-Site-Id" content="1327d789-ee4c-4177-6868-a8ae5dadd191"/>
+    <meta http-equiv="X-Wix-Application-Instance-Id" content="1327d789-e9bc-8590-d1e4-63e95080c3f2"/>
+    <meta http-equiv="X-Wix-Published-Version" content="2"/>
+    <meta http-equiv="etag" content="a88d948e0c81a549076b9818c299b1a8"/>
+    <meta name="robots" content="noindex"/>
+    <meta property="og:title" content="site-action-citoyen"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="http://louissimard11.wix.com/site-action-citoyen"/>
+    <meta property="og:site_name" content="site-action-citoyen"/>
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
+
+    <meta id="wixMobileViewport" name="viewport" content="minimum-scale=0.25, maximum-scale=1.2"/>
+    <!--
+   IMPORTANT! LOOKING FOR THIS SITE’S SEO CONTENT?
+
+   This site's SEO content, such as meta tags and headers, is not here.
+   This is because search engines, like Google, actually crawl the site's homepage via http://louissimard11.wix.com/site-action-citoyen?_escaped_fragment_=.
+
+   Internal pages, like “MEDIA”, also have their own special search engine versions, for example: http://louissimard11.wix.com/site-action-citoyen?_escaped_fragment_=media%2Fc1w1e
+   If you’re looking for this site’s SEO content, that’s where you can view it.
+
+   Want more information about Ajax page crawling? Read Google's explanation here: http://bit.ly/ajaxcrawling
+-->
+    <!-- META DATA -->
+    <script type="text/javascript">
+        var rendererModel = {"debugMode":"nodebug","previewMode":false,"serviceMappings":{"1":{"idInMetaSite":1,"idInApp":"1327d789-e9bc-8590-d1e4-63e95080c3f2","applicationType":"HtmlWeb"}},"metaSiteId":"1327d789-ee4c-4177-6868-a8ae5dadd191","premiumFeatures":[],"siteId":"1327d789-e9bc-8590-d1e4-63e95080c3f2","userId":"78dadaa3-aac6-42a0-aa02-ad4fd801cbb5","published":true,"applicationType":"HtmlWeb","documentType":"UGC","siteTitleSEO":"site-action-citoyen","clientSpecMap":{"11":{"type":"sitemembers","applicationId":11,"collectionType":"Open","smtoken":"1fa701aaa3dbe00be30d4da137ff8cb706a0d6d4026344b5aa44729133b9f5ae4129c54007c861848a3158836322125b0c5a3903aa550ca63040b609042b7109b9d68c25de59c786724215c3eb90b017849568106a5dd40c669ce9fcbda778e1","smcollectionId":"1327d789-efce-abc7-5322-09bf37b597ee"},"0":{"type":"appbuilder","applicationId":0,"appDefinitionId":"3d590cbc-4907-4cc4-b0b1-ddf2c5edf297","instanceId":"1327d789-ef0b-49b4-6337-6fe763663326"}},"runningExperiments":{"CorsContactForm":"New","verifyPremium":"New","NoAnchorsFromServer":"New","ViewerRefactor":"New","PageHeightFix":"New","WixAppsDirection":"new","EcomExp1":"New","mobileSiteWidth":"New","BackgroundSRZ":"New","JITPageLoader":"New","SMPasswordPoundCharFix":"New","ImageLoadPerformance":"New","RichTextPresets":"New","WixAppsPage":"new","TPA3DGallery":"new","MobileSliderGallery":"New","RefreshFrameOnTransition":"New","useClientNewDeployViewer":"true","EcomTax":"New","NewFetchPolicy":"New","NBC_WCD":"new","tpaartifact":"new","mediaGalleryFrame":"New","WixAppsLayoutFixes":"new","AutoPlayFix":"New","SyncWixify":"New","QuickActionsMenus":"New","SMOwnerLogoutFix":"New","MobileEcomViews":"New","EcomSocial":"New","PageDuplicateLinksFix":"new","MobileBackgroundSize":"New","MainPageHash":"New","WixAppsContentResize":"new","QuickActionsHideSubPagesOfHiddenParent":"New","bgimagestrip":"new","WixAppsApplicationData":"New","RenderPerf":"new","PageManagerRefactor":"new","FixMissingSkins":"New","fontsproject":"New","RichText2":"New","NBC_VerticalLine":"New","SiteVersion":"new","MobileZoom":"New","LimitRecursiveDDM":"New","fontsproject1":"New","MobileViewerQuickActions":"New","GalleryFlick":"New","InnerStylesFix":"New","custompreviewbehavior":"new","ActivityReporter":"new","DynamicCSM":"new","MaximumComponentHeight":"New","twitterapipatch":"new","SiteMembersCommon":"New","safari5menufix":"New","SetHashRefactor":"New","firsttimevisitoreditoruipreview":"New","PageLoadReport":"New","SiteJumpFix":"new","FooterHeightFix":"New","Mobileviewercomponents":"New","PostSignupUrlParam":"New","HtmlComponentRelativePath":"New","EcomRepo":"New","FixViewport":"new","tpanewsettings":"new","NBC_HorizontalLine":"New","URM4":"New","AutoResizeEvent":"new","synclayout":"New","SMForgot_Password":"New","FacebookCommentFix":"New","tpaglued":"new","pixelPerfectEditorTabs":"New"},"languageCode":"en","scriptsCacheKiller":1,"siteMetaData":{"hasMobileStructure":false,"quickActions":{"socialLinks":[],"colorScheme":"dark","configuration":{"quickActionsMenuEnabled":true,"navigationMenuEnabled":true,"phoneEnabled":false,"emailEnabled":false,"addressEnabled":false,"socialLinksEnabled":false}}}};
+        var publicModel = {"externalBaseUrl":"http://louissimard11.wix.com/site-action-citoyen","domain":"wix.com","premiumFeatures":[],"favicon":"","suppressTrackingCookies":false,"pageList":{"masterPage":["http://static.wixstatic.com/sites/78dada_0c722615c8391fc23c9947a7bd4b8873_2.json.z?v=2","http://archive.wix.com/sites/78dada_0c722615c8391fc23c9947a7bd4b8873_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_0c722615c8391fc23c9947a7bd4b8873_2.json"],"pages":[{"pageId":"c1w1e","title":"MEDIA","urls":["http://static.wixstatic.com/sites/78dada_2c4f4f35c8cfc0f33cffb8cc0fd87b5a_2.json.z?v=2","http://archive.wix.com/sites/78dada_2c4f4f35c8cfc0f33cffb8cc0fd87b5a_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_2c4f4f35c8cfc0f33cffb8cc0fd87b5a_2.json"]},{"pageId":"mainPage","title":"HOME","urls":["http://static.wixstatic.com/sites/78dada_43ab8492db2dc715d3b9b2968957cb22_2.json.z?v=2","http://archive.wix.com/sites/78dada_43ab8492db2dc715d3b9b2968957cb22_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_43ab8492db2dc715d3b9b2968957cb22_2.json"]},{"pageId":"c1adp","title":"BIO","urls":["http://static.wixstatic.com/sites/78dada_6696e7d04292e040b89fc0a35db79215_2.json.z?v=2","http://archive.wix.com/sites/78dada_6696e7d04292e040b89fc0a35db79215_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_6696e7d04292e040b89fc0a35db79215_2.json"]},{"pageId":"aboutPage","title":"NEWS","urls":["http://static.wixstatic.com/sites/78dada_8a6704bf92ed582e67b43a38fcb9f6c7_2.json.z?v=2","http://archive.wix.com/sites/78dada_8a6704bf92ed582e67b43a38fcb9f6c7_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_8a6704bf92ed582e67b43a38fcb9f6c7_2.json"]},{"pageId":"galleryPage","title":"GIGS","urls":["http://static.wixstatic.com/sites/78dada_aa71ae6be9ebcc5156241354380acd54_2.json.z?v=2","http://archive.wix.com/sites/78dada_aa71ae6be9ebcc5156241354380acd54_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_aa71ae6be9ebcc5156241354380acd54_2.json"]},{"pageId":"ServicesPage-bmmaq7o0","title":"CONTACT","urls":["http://static.wixstatic.com/sites/78dada_b6e501ee0b55b061f23e6b9f45208186_2.json.z?v=2","http://archive.wix.com/sites/78dada_b6e501ee0b55b061f23e6b9f45208186_2.json.z?v=2","http://publicorigin.wix.com/wix-public-html-renderer/page/78dada_b6e501ee0b55b061f23e6b9f45208186_2.json"]}],"mainPageId":"mainPage"},"siteRevision":2,"timeSincePublish":17262,"adaptiveMobileOn":false};
+        var serviceTopology = {"serverName":"app32.aus.wixpress.com","cacheKillerVersion":"1","staticServerUrl":"http://static.wix.com/","usersScriptsRoot":"http://static.wix.com/services/wix-users/2.222.0","biServerUrl":"http://frog.wix.com/","userServerUrl":"http://users.wix.com/","billingServerUrl":"http://premium.wix.com/","mediaRootUrl":"http://static.wixstatic.com/","logServerUrl":"http://frog.wix.com/plebs","monitoringServerUrl":"http://TODO/","usersClientApiUrl":"https://users.wix.com/wix-users","publicStaticBaseUri":"http://static.wix.com/services/wix-public/1.36.0","basePublicUrl":"http://www.wix.com/","postLoginUrl":"http://www.wix.com/create/my-account","postSignUpUrl":"http://www.wix.com/new/account","baseDomain":"wix.com","staticMediaUrl":"http://static.wixstatic.com/media","staticAudioUrl":"http://media.wix.com/mp3","emailServer":"http://assets.wix.com/common-services/notification/invoke","blobUrl":"http://static.parastorage.com/wix_blob","htmlEditorUrl":"http://editor.wix.com/html","siteMembersUrl":"https://users.wix.com/wix-sm","scriptsLocationMap":{"ck-editor":"http://static.parastorage.com/services/ck-editor/1.36.0","bootstrap":"http://static.parastorage.com/services/bootstrap/2.604.6","sitemembers":"http://static.parastorage.com/services/sm-js-sdk/1.27.0","ecommerce":"http://static.parastorage.com/services/ecommerce/1.59.0","langs":"http://static.parastorage.com/services/langs/2.168.0","wixapps":"http://static.parastorage.com/services/wixapps/2.233.3","verifypremium":"http://static.parastorage.com/services/experiments/verifypremium/1.7.0","web":"http://static.parastorage.com/services/web/2.604.6","core":"http://static.parastorage.com/services/core/2.604.6","tpa":"http://static.parastorage.com/services/tpa/2.583.0","skins":"http://static.parastorage.com/services/skins/2.604.6"},"developerMode":false,"userFilesUrl":"http://static.wix.com/","staticHTMLComponentUrl":"http://static.usrfiles.com/","secured":false,"ecommerceCheckoutUrl":"https://www.safer-checkout.com/","publicStaticsUrl":"http://static.wix.com/services/wix-public/1.36.0","staticDocsUrl":"http://media.wix.com/ugd"};
+        var siteHeader = {"id":"1327d789-e9bc-8590-d1e4-63e95080c3f2", "userId":"78dadaa3-aac6-42a0-aa02-ad4fd801cbb5"};
+        var siteId = siteHeader.id;
+
+        var configUrls = serviceTopology;
+
+        var debugMode = "nodebug";
+        var viewMode = (rendererModel.previewMode) ? 'preview' : 'site';
+
+        var googleAnalytics = ""
+            ;
+
+    </script>
+
+
+    <meta name="fragment" content="!"/>
+    <!-- DATA -->
+    <script type="text/javascript">
+        var adData = {"topLabel":"<span class=\"smallMusa\">(Wix-Logo) </span>Create a <span class=\"smallLogo\">Wix</span> site!","topContent":"100s of templates<br />No coding needed<br /><span class=\"emphasis spacer\">Start now >></span>","footerLabel":"<div class=\"adFootBox\"><div class=\"siteBanner\" ><div class=\"siteBanner\"><div class=\"wrapper\"><div class=\"bigMusa\">(Wix Logo)</div><div class=\"txt shd\" style=\"color:#fff\">This site was created by </div> <div class=\"txt shd\"><a  href=\"http://www.wix.com?utm_campaign=vir_wixad_live\" style=\"color:#fff\"> WIX.com. </a></div> <div class=\"txt shd\" style=\"color:#fff\"> Create your own for FREE <span class=\"emphasis\"> >></span></div></div></div></div></div>","adUrl":"http://www.wix.com/html5webbuilder/400?utm_campaign=vir_wixad_live"};
+        var usersDomain = "https://users.wix.com//wix-users";
+    </script>
+
+
+
+    <script src="http://static.wix.com/services/wix-users/2.222.0/client/js/userApi_v2.js?cacheKiller=1"></script>
+
+
+
+    <script type="text/javascript">
+        var userApi = UserApi.getInstance().init({
+            "usersDomain":"http://users.wix.com//wix-users",
+            "corsEnabled":false,
+            "dontHandShake":true,
+            "urlThatUserRedirectedFrom":"$"
+        });
+    </script>
+
+</head>
+<body>
+<script type="text/javascript">var NREUMQ=NREUMQ||[];NREUMQ.push(["mark","firstbyte",new Date().getTime()]);</script>
+
+<link rel="stylesheet" type="text/css" href="http://static.wix.com/services/web/2.604.6/css/wysiwyg/preloader.css">
+
+<div id="viewer_preloader">
+    <p>Loading</p>
+    <h2></h2>
+    <div id="preloader"></div>
+</div>
+
+<script type="text/javascript" src="http://static.wix.com/services/web/2.604.6/javascript/wysiwyg/viewer/mobile_utils.js"></script>
+<script type="text/javascript" src="http://static.wix.com/services/web/2.604.6/javascript/wysiwyg/viewer/preloader.js"></script>
+
+
+<link rel="stylesheet" type="text/css" href="http://static.wix.com/services/web/2.604.6/css/wysiwyg/quick_actions.css">
+
+<style type="text/css">
+    .dark .quick-actions-sprite,
+    .dark .quick-actions-sprite-before:before,
+    .dark .quick-actions-sprite-after:after{
+        background-image : url(http://static.wix.com/services/web/2.604.6/images/wysiwyg/viewer/quickAction_darkSprite.png);
+    }
+    .dark .quick-actions-bg-gradient{
+        background-image : url(http://static.wix.com/services/web/2.604.6/images/wysiwyg/viewer/quickActions_dark_bg.png);
+    }
+
+    .light .quick-actions-sprite,
+    .light .quick-actions-sprite-before:before,
+    .light .quick-actions-sprite-after:after{
+        background-image : url(http://static.wix.com/services/web/2.604.6/images/wysiwyg/viewer/quickAction_lightSprite.png);
+    }
+    .light .quick-actions-bg-gradient{
+        background-image : url(http://static.wix.com/services/web/2.604.6/images/wysiwyg/viewer/quickAction_lightSprite.png);l
+    }
+
+    .quick-actions-drop-up-x {
+        background-image: url(http://static.wix.com/services/web/2.604.6/images/wysiwyg/viewer/x.png);
+    }
+
+</style>
+
+<div id="quick-actions-menus-container">
+    <div id="qa-drop-up-pages" class="quick-actions-drop-up-wrapper">
+        <div class="quick-actions-drop-up-back"></div>
+        <div class="quick-actions-drop-up quick-actions-drop-up-open">
+            <div class="quick-actions-drop-up-header">
+                <span class='quick-actions-drop-up-title'>Pages</span>
+                <span class='quick-actions-drop-up-x'></span>
+            </div>
+
+            <ul id="qa-drop-up-pages-list">
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+
+<div id="quick-actions" class="quick-actions dark">
+    <div id="quick-actions-drag-handle" class="quick-actions-drag-handle quick-actions-sprite">
+        &nbsp
+    </div>
+
+    <div id="quick-actions-bar" class="quick-actions-bar quick-actions-bg-gradient">
+
+        <ul id="qa-menu" class="quick-actions-menu">
+            <li id="quick-actions-pages-button" class="qa-new-menus-hide quick-actions-item quick-actions-site-nav quick-actions-sprite disabled">
+                <div class="quick-actions-site-nav-button quick-actions-sprite">
+                    <a class="quick-actions-button quick-actions-sprite"></a>
+                </div>
+            </li>
+
+
+        </ul>
+
+    </div>
+</div>
+
+<script src="http://static.wix.com/services/web/2.604.6/libs/hammer.min.js"></script>
+<script src="http://static.wix.com/services/web/2.604.6/javascript/wysiwyg/viewer/quick_actions_new_menus.js"></script>
+<script>
+    if (MobileUtils.isMobile() && MobileUtils.isViewportOpen()){
+        WQuickActions.element.style.visibility = "visible";
+        WQuickActions.initialize(
+            "",
+            "",
+            ""
+        );
+    } else if(WQuickActions && WQuickActions.element){
+        WQuickActions.element.style.display = 'none';
+    }
+</script>
+
+
+<!-- debug mode=nodebug -->
+<!-- anc2 -->
+<script type="text/javascript">
+    var anchors = {};
+
+
+</script>
+
+<script src="http://static.parastorage.com/services/bootstrap/2.604.6/javascript/bootstrap.min.js"></script>
+<script src="http://static.parastorage.com/services/web/2.604.6/deployviewer.min.js"></script>
+
+
+
+
+<div comp="wysiwyg.viewer.components.WixAds" skin="wysiwyg.viewer.skins.wixadsskins.WixAdsWebSkin" id="wixFooter"></div>
+
+
+
+<script>
+    window.define && define.resource('status.structure.loaded', true);
+</script>
+
+
+
+<script type="text/javascript">if (!NREUMQ.f) { NREUMQ.f=function() {NREUMQ.push(["load",new Date().getTime()]);var e=document.createElement("script");e.type="text/javascript";e.src=(("http:"===document.location.protocol)?"http:":"https:") + "//" + "js-agent.newrelic.com/nr-100.js";document.body.appendChild(e);if(NREUMQ.a)NREUMQ.a();};NREUMQ.a=window.onload;window.onload=NREUMQ.f;};NREUMQ.push(["nrfj","beacon-6.newrelic.com","c99d7f1ab0","1963269,1963270","ZFAHNkNYXUBQVEUKXF0aKDRyFmRWU39FDl9hUAsGVEtWQR9FVA1XVkc=",0,6,new Date().getTime(),"","","","",""]);</script>
+</body>
+</html>
